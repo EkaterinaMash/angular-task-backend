74a02a5f53bb124d7dbf82e9921efc4d7a3e7a64
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2023-08-01 12:24:06 +0300

    feat: add link to mongo db

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,8 +6,9 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://ekaterina:nwu3VmN9pjbnl04S@cluster0.hthemi3.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
+      console.log(process.env.PORT || PORT);
       console.log('Сервер ожидает подключения...');
     })
   } catch (error) {
